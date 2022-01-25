c3ff75f047ed236800e5ad0522fbbc0906615219
Lasya0500 <gottupalilasya@gmail.com>
2022-01-25 18:07:49 +0530

    second submission

---
+++ day2Hello(for loop)/day2Hello(for loop)/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day2Hello_for_loop_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "Hello";
+
+            for(int i = 1 ; i <= 5; i++)
+            {
+                Console.WriteLine(word);
+
+                
+            }
+            Console.ReadLine(); 
+        }
+    }
+}
